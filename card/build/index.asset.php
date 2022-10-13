0e7b767cfb2cb11a36161ce9423cc8bd1adf62fc
AndFu <andrewph7077992966+1@gmail.com>
2022-10-13 14:21:23 -0500

    Fixed verification bug

--- card/build/index.asset.php
+++ card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '08e8b478d143e99ca053');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '3a1f928f5cdf8d8224c3');
